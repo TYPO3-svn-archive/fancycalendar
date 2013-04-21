7220f888b84e921c5472109382f5a2b73056cd8e
schmitzal <schmitzal@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-04-21 12:15:39 +0000

    Initial checkin
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fancycalendar/trunk@74691 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ fancycalendar/trunk/Classes/Domain/Repository/EventRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Alessandro Schmitz <me@alessandro-schmitz.de>
+ *  
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ *
+ *
+ * @package fancycalendar
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ *
+ */
+class Tx_Fancycalendar_Domain_Repository_EventRepository extends Tx_Extbase_Persistence_Repository {
+
+    /**
+     * @see http://api.typo3.org/typo3cms/47/html/class_tx___extbase___persistence___typo3_query_settings.html
+     */
+    public function __construct() {
+        parent::__construct();
+
+        /** @var $newDefaultQuerySettings Tx_Extbase_Persistence_Typo3QuerySettings */
+        $newDefaultQuerySettings = t3lib_div::makeInstance('Tx_Extbase_Persistence_Typo3QuerySettings');
+        $newDefaultQuerySettings->setRespectStoragePage(FALSE);
+        $this->setDefaultQuerySettings($newDefaultQuerySettings);
+    }
+}
+?>
